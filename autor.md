9cedf970fd62d05d4caf6e89c767c32bcf7b0e04
Matheus <matheus.r.slv@gmail.com>
2021-06-17 17:54:40 +0000

    update ponto em retangulo 1

--- autor.md
+++ autor.md
@@ -10,12 +10,12 @@ Preencha os dados abaixo para identificar a autoria do trabalho.
 
 - [x] Negativos 5
 - [x] Soma Vizinhos
-- [ ] Intervalos
+- [x] Intervalos
 - [x] Fibonacci
 - [x] Minmax
 - [x] Inverter
 - [ ] Filtragem
-- [ ] Ponto em Retângulo 1
+- [x] Ponto em Retângulo 1
 - [ ] Ponto em Retângulo 2
 
 
--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -8,6 +8,15 @@ location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
     // TODO: Coloque aqui seu código.
 
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
+    if(P.x > IE.x and P.x < SD.x and P.y > IE.y and P.y < SD.y){
+        return location_t::INSIDE;
+    }
+    else if( (P.x >= IE.x && P.x <= SD.x && P.y == IE.y) || (P.x >= IE.x && P.x <= SD.x && P.y == SD.y) ){
+        return location_t::BORDER;
+    }
+    else if( (P.x == IE.x && P.y >= IE.y && P.y <= SD.y) || (P.x == SD.x && P.y >= IE.y && P.y <= SD.y) ){ 
+        return location_t::BORDER;
+    }
+
     return location_t::OUTSIDE;
 }
