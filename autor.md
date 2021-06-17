30f4b5239b304723e313822543b94b2482ae397e
Matheus <matheus.r.slv@gmail.com>
2021-06-17 23:40:48 +0000

    update ponto em retangulo 2

--- autor.md
+++ autor.md
@@ -14,7 +14,6 @@ Preencha os dados abaixo para identificar a autoria do trabalho.
 - [x] Fibonacci
 - [x] Minmax
 - [x] Inverter
-- [ ] Filtragem
 - [x] Ponto em Retângulo 1
 - [ ] Ponto em Retângulo 2
 
--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -21,7 +21,63 @@ using std::max;
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int x1, y1, x2, y2, x, y;
+    Ponto R1;
+    Ponto R2, P;
+
+    while(cin >> std::ws >> x1 >> y2 >> x2 >> y2 >> x >> y){
+          if( (x1 == x2) && (y1 == y2) ){
+              cout << "invalid" << endl;
+          }
+          else if( y1 < y2 ){ //x1,y1 bottom
+              if(x1 < x2){ //x1,y1 bottom left 
+                  R1.x = x1, R1.y = y1;
+                  if(x1==y2){ //x2,y2 top left
+                  
+                  }
+                  else{ //x2,y2 top right
+                      R2.x = x2, R2.y = y2;
+                  }
+
+              }
+              else if(x1 > x2){ //x1,y1 bottom right
+                  if(x1==y2){ //x2,y2 top right
+
+                  }
+                  else{ //x2,y2 top left
+                  }
+              }
+          }
+          else if( y1 > y2 ){ //x1,y1 top
+              if(x1 < x2){ //x1,y1 top left 
+                  if(x1==y2){  //x2,y2 bottom left
+
+                  }
+                  else{  //x2,y2 bottom right
+                      
+                  }
+              }
+              else if(x1 > x2){ //x1,y1 top right
+
+              }
+          }
+          else{
+              R1.x = x1, R1.y = y1;
+              R2.x = x2, R2.y = y2;
+          }
+
+          P.x = x, P.y = y;
+
+          if(pt_in_rect(  R1,  R2,  P )==0){
+              cout << "inside" << endl;
+          }
+          else if(pt_in_rect(  R1,  R2,  P )==1){
+              cout << "border" << endl;
+          }
+          else if(pt_in_rect(  R1,  R2,  P )==0){
+              cout << "outside" << endl;
+          }
+    } 
 
     return 0;
 }
